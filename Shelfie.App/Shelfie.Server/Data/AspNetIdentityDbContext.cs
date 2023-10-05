80bb98e596dce9b00ddcf0ac443fdc2450c54e24
Monika Maczkowiak <monika.maczkowiak@xebia.com>
2023-10-05 11:41:33 +0200

    wip

---
+++ Shelfie.App/Shelfie.Server/Data/AspNetIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shelfie.Server.Data
+{
+    public class AspNetIdentityDbContext : IdentityDbContext
+    {
+        public AspNetIdentityDbContext(DbContextOptions<AspNetIdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- Shelfie.App/Shelfie.Server/Program.cs
+++ Shelfie.App/Shelfie.Server/Program.cs
@@ -1,26 +1,51 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Shelfie.Server;
+using Shelfie.Server.Data;
+
+var seed = args.Contains("/seed");
+if(seed)
+{
+    args = args.Except(new[] {"/seed"}).ToArray();
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (seed)
+{    
+    SeedData.EnsureSeedData(connectionString);
+}
+
 var assembly = typeof(Program).Assembly.GetName().Name;
 
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options => 
+    options.UseSqlServer(connectionString,
+        sql => sql.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
         options.ConfigureDbContext = b =>
-            b.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+            b.UseSqlServer(connectionString,
                            sql => sql.MigrationsAssembly(assembly));
     })
     .AddOperationalStore(options =>
     {
         options.ConfigureDbContext = b =>
-            b.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+            b.UseSqlServer(connectionString,
                            sql => sql.MigrationsAssembly(assembly));
     })
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
+app.UseRouting();
 app.UseIdentityServer();
 
 app.Run();
